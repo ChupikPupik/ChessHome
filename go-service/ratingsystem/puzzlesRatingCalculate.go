a3d06df85d51e046d0ef5efc11b5e7701b34e77b
ChupikPupik <mcc@devnews.site>
2026-09-24 21:09:43 +0300

    update rating formula

--- go-service/ratingsystem/puzzlesRatingCalculate.go
+++ go-service/ratingsystem/puzzlesRatingCalculate.go
@@ -6,7 +6,9 @@ import (
 
 func CalculatePuzzleRating(ratingPlayer float64, puzzleRating float64, res float64) float64 {
 	expected := 1.0 / (1.0 + math.Pow(10, (puzzleRating-ratingPlayer)/400.0))
-	newUserRating := ratingPlayer + 32.0*(res-expected)
+	delta := 32.0 * (res - expected)
+	delta = math.Max(-40, math.Min(40, delta))
 
+	newUserRating := math.Max(100, ratingPlayer+delta)
 	return newUserRating
 }
